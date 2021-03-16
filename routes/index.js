29ab16d2cbfc49e900bdbf31e15e23dca81c0710
hanjuren <juren528@gmail.com>
2021-03-16 14:36:27 +0900

    Get login

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
 });
 
 module.exports = router;
