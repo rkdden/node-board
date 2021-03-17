81257d5465b834257c442a5d46f469c0374fba1b
SY <12tndbs12@naver.com>
2021-03-17 16:12:50 +0900

    로컬 로그인 구현

---
+++ passport/index.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const User = require('../models/user');
+
+module.exports = () => {
+    passport.serializeUser((user, done) => {
+        done(null, user.id);    // 세션에 user의 id만 저장
+    });
+
+    passport.deserializeUser((id, done) => {
+        User.findOne({
+            where: { id },
+            attributes: ['id', 'email', 'name'],
+        })
+            .then(user => done(null, user))
+            .catch(err => done(err));
+    });
+
+    local();
+    // kakao();
+};
