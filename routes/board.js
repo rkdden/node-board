ad9b93b5d5a17ba3a3de5ebe86fb1dbfa6b63671
hanjuren <juren528@gmail.com>
2021-03-21 16:01:55 +0900

    게시글 가져오기 작성중

--- routes/board.js
+++ routes/board.js
@@ -1,2 +1,31 @@
-// 게시글 라우터
-// 게시판 만들기
+const express = require('express');
+const User = require('../models/user');
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    // 최신순 , 댓글순, 조회순, 추천순
+    try{
+        if(req.query.condition === null) { // 최신순
+            const posts = await Post.findAll({
+                include: [
+                    {
+                        model: User,
+                        attributes: ['name'],
+                    },
+                    {
+                        model: Comment,
+                    },
+                ],
+                order: ['id', 'DESC'],
+            });
+        }
+
+    } catch (error) {
+
+    }
+});
+
+module.exports = router;
