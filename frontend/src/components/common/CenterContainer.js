8c9227541fe010edfcc7b097aa916713a902441a
SY <12tndbs12@naver.com>
2021-05-26 21:22:39 +0900

    컨테이너 분리

---
+++ frontend/src/components/common/CenterContainer.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+// 전체 페이지 70% 래퍼
+const CenterContainer = styled.div`
+    width: 70%;
+    height: 100%;
+    background: gray;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export default CenterContainer; 
